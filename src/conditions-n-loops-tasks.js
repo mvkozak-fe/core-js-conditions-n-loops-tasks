966e33ba97139537c72092105372c8058c53a5e5
mvkozak-fe <mvkozak.fe@gmail.com>
2024-02-11 02:17:39 +0300

    feat: add convertNumberToString task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -139,8 +139,58 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let str = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (numberStr[i]) {
+      case '0':
+        str += 'zero ';
+        break;
+      case '1':
+        str += 'one ';
+        break;
+      case '2':
+        str += 'two ';
+        break;
+      case '3':
+        str += 'three ';
+        break;
+      case '4':
+        str += 'four ';
+        break;
+      case '5':
+        str += 'five ';
+        break;
+      case '6':
+        str += 'six ';
+        break;
+      case '7':
+        str += 'seven ';
+        break;
+      case '8':
+        str += 'eight ';
+        break;
+      case '9':
+        str += 'nine ';
+        break;
+      case '-':
+        str += 'minus ';
+        break;
+      case ',':
+        str += 'point ';
+        break;
+      case '.':
+        str += 'point ';
+        break;
+      default:
+        break;
+    }
+  }
+  let res = '';
+  for (let i = 0; i < str.length - 1; i += 1) {
+    res += str[i];
+  }
+  return res;
 }
 
 /**
